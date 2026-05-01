73ba5a51baabca68135ff9747cade3e99a4eac46
Denys <den.linevych@gmail.com>
2026-05-01 13:59:30 +0300

    chore: project setup

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplexMethodApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
